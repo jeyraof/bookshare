c4ca762392eff1cdb112a6cfae8bdaa7c5738624
Jaeyoung Lee <jaeyoung@monodiary.net>
2015-05-05 12:17:42 +0900

    Add python-social-auth's urls settings.

--- bookshare/urls.py
+++ bookshare/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
 ]

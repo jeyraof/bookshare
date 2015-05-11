9973fbefa2d7e113f64ffe7cc51b600e877f019d
Jaeyoung Lee <jaeyoung@monodiary.net>
2015-05-11 17:30:13 +0900

    Apply static files

--- bookshare/settings/base.py
+++ bookshare/settings/base.py
@@ -86,6 +86,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR + 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 

48d7a9aa80a360e54b9ff64dc720657478beaa3e
Jaeyoung Lee <jaeyoung@monodiary.net>
2015-05-14 12:09:21 +0900

    Render react-search

--- book/views.py
+++ book/views.py
@@ -1,29 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from django.http import (JsonResponse,
-                         HttpResponseNotAllowed, HttpResponseBadRequest)
-from django.views.generic import TemplateView
-from django.views.decorators.csrf import csrf_exempt
-from book.models import Book
+from bookshare.utils import react_render
 
 
-class BookDetail(TemplateView):
-    template_name = 'book/detail.html'
-
-    def get(self, request, *args, **kwargs):
-        return super(BookDetail, self).get(request, *args, **kwargs)
-
-
-@csrf_exempt
-def search_books(request):
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(u'잘못된 접근입니다.')
-
-    q = request.POST.get('q', None)
-    if not q:
-        return HttpResponseBadRequest(u'인자가 부족합니다.')
-
-    search_type = request.POST.get('type', 'all')
-    books = Book.search(search_type=search_type,
-                        q=q)
-    return JsonResponse({'books': map(lambda b: b.to_json(), books)})
+def search(request):
+    return react_render(request, 'app/search.js', {})
